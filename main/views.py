e59731ae9b58cc561c265917cdc4d382bc21ef23
sachin064 <sachinbgsachin3@gmail.com>
2019-07-11 18:24:08 +0530

    first commit

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.template.loader import get_template
+from django.core.mail import EmailMessage
+from django.shortcuts import redirect
+from django.contrib import messages
+
+
+# Create your views here.
+def home(request):
+	return render(request, 'main/index.html')
+
+def about(request):
+	return render(request, 'about/about.html')
